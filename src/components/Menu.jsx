24acdb2c06fc622460f8655fd769a2aefb32b653
Laylla Emily <layllandradef@live.com>
2022-12-05 23:50:05 -0300

    Criando css do componente menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,23 +1,28 @@
 import { Link } from 'react-router-dom'
+import styles from '../styles/components/menu.module.css'
+import logoImg from '../assets/logo.jpeg'
 
 export function Menu() {
   return(
-    <ul>
-      <li>
-        <Link to="/">Inicio</Link>
+    <div className={styles.menuContainer}>
+    <img className={styles.logo} src={logoImg} />
+    <ul className={styles.menuContainer}>
+      <li className={styles.menuItem}>
+        <Link className={styles.menuLink} to="/">Inicio</Link>
+      </li >
+      <li className={styles.menuItem}>
+        <Link className={styles.menuLink} to="/sobre">Sobre nós</Link>
       </li>
-      <li>
-        <Link to="/sobre">Sobre nós</Link>
+      <li className={styles.menuItem}>
+        <Link className={styles.menuLink} to="/doe">Faça sua doação</Link>
       </li>
-      <li>
-        <Link to="/doe">Faça sua doação</Link>
+      <li className={styles.menuItem}>
+        <Link className={styles.menuLink} to="/queroreceber">Quero receber</Link>
       </li>
-      <li>
-        <Link to="/queroreceber">Quero receber</Link>
-      </li>
-      <li>
-        <Link to="/contato">Contato</Link>
+      <li className={styles.menuItem}>
+        <Link className={styles.menuLink} to="/contato">Contato</Link>
       </li>
     </ul>
+    </div>
   )
 }
