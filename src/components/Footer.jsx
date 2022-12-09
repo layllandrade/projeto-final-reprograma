3f4e5774d6511d25db9232b82b48891d24c1da34
Laylla Emily <layllandradef@live.com>
2022-12-08 21:38:35 -0300

    Estilizando o footer e adicionando links e icones, para o github e linkedin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,25 @@
 import styles from '../styles/components/footer.module.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 
 export function Footer() {
     return (
       <footer className={styles.footerContainer}>
-        <p className={styles.footerText}>
-          Feito com ♡ por Laylla Andrade
-        </p>
+        <div className={styles.footerTextContainer}>
+          <p className={styles.footerText}>
+            Feito com ♡ por Laylla Andrade
+          </p>
+        </div>
+        <div className={styles.footerIconsContainer}>
+          <div className={styles.footerIcons}>
+            <FontAwesomeIcon icon={faLinkedin} size="3x" color="white" />
+            <a className={styles.footerIconsLink} href="https://www.linkedin.com/in/laylla-farias/">/laylla-farias</a>
+          </div>
+          <div className={styles.footerIcons}>
+            <FontAwesomeIcon icon={faGithubSquare} size="3x" color="white"/>
+            <a className={styles.footerIconsLink} href="https://github.com/layllandrade">/layllandrade</a>
+          </div>
+        </div>
       </footer>
     )
   }
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -5,7 +5,7 @@ import transImg from '../assets/trans.png'
 export function Inicio() {
     return(
       <div className={styles.inicioContainer}>
-        <Header titulo="TRANSforma Campina" descricao="O projeto tranforma" imagem={transImg}/>
+        <Header titulo="TRANSforma Campina" descricao="O projeto Transforma Campina é um trabalho voluntário, que foi criado no intuito de ajudar pessoas que estão no inicio da transição de gênero e em situação de vulnerabilidade econômica. O projeto tem como proposito arrecadar doações de peças de roupas, calçados e acessórios. Fazendo com que os beneficiados consigam se vestir da forma como o qual se identificam, trazendo assim mais auto estima, apoio e auto confiança para essas pessoas nesse período." imagem={transImg}/>
       </div>
     )
 }
